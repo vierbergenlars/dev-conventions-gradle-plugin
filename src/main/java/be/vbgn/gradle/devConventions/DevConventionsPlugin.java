1127521e42ce7da7a8d7ab9cc8fe72a340a0c6eb
Lars Vierbergen <lars@vbgn.be>
2020-10-24 14:27:11 +0200

    Initial commit

---
+++ src/main/java/be/vbgn/gradle/devConventions/DevConventionsPlugin.java
@@ -0,0 +1,12 @@
+package be.vbgn.gradle.devConventions;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class DevConventionsPlugin implements Plugin<Project> {
+
+    @Override
+    public void apply(Project project) {
+        
+    }
+}
