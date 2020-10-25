5ea5a42bfab6ec6a5925fdbfa262bdbe86d51001
Lars Vierbergen <lars@vbgn.be>
2020-10-25 17:59:33 +0100

    Make sonarqube depend on all JacocoReport tasks

--- src/main/groovy/be/vbgn/gradle/devConventions/conventions/impl/JacocoSonarqubeConvention.groovy
+++ src/main/groovy/be/vbgn/gradle/devConventions/conventions/impl/JacocoSonarqubeConvention.groovy
@@ -2,20 +2,19 @@ package be.vbgn.gradle.devConventions.conventions.impl
 
 import be.vbgn.gradle.devConventions.conventions.Convention
 import org.gradle.api.Project
+import org.gradle.testing.jacoco.tasks.JacocoReport
 
 class JacocoSonarqubeConvention implements Convention {
     @Override
     void apply(Project project) {
-        project.plugins.withId("java") {
-            project.plugins.withId("jacoco") {
-                project.plugins.withId("org.sonarqube") {
-                    project.tasks.named("sonarqube").configure {
-                        dependsOn("jacocoTestReport", "check")
-                    }
-                    project.tasks.named("jacocoTestReport").configure {
-                        reports {
-                            xml.enabled = true
-                        }
+        project.plugins.withId("jacoco") {
+            project.plugins.withId("org.sonarqube") {
+                project.tasks.named("sonarqube").configure {
+                    dependsOn(project.tasks.withType(JacocoReport.class), "check")
+                }
+                project.tasks.withType(JacocoReport.class).configureEach {
+                    reports {
+                        xml.enabled = true
                     }
                 }
             }
